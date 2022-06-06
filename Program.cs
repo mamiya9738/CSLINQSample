b04bc858c44c222a330ed3464c1747aa44190908
mamiya9738 <gogo9738@gmail.com>
2022-06-06 20:45:53 +0900

    意図した通りに動くよう修正

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@ var listA = new List<A>(){
         new A(){ Key = 3 , Name = "Name3",},
         new A(){ Key = 4 , Name = "Name4",},
         new A(){ Key = 5 , Name = "Name5",},
+        new A(){ Key = 6 , Name = "Name6",},
+        new A(){ Key = 7 , Name = "Name7",},
+        new A(){ Key = 8 , Name = "Name8",},
+        new A(){ Key = 9 , Name = "Name9",},
 };
 
 // テストデータB
@@ -14,10 +18,6 @@ var listB = new List<A>(){
         new A(){ Key = 3 , Name = "Name3'",},
         new A(){ Key = 4 , Name = "Name4'",},
         new A(){ Key = 5 , Name = "Name5'",},
-        new A(){ Key = 6 , Name = "Name6'",},
-        new A(){ Key = 7 , Name = "Name7'",},
-        new A(){ Key = 8 , Name = "Name8'",},
-        new A(){ Key = 9 , Name = "Name9'",},
 };
 
 // A And B かつ 3の倍数の値のみを取得する
@@ -25,16 +25,14 @@ var listTest =  ( from right in listA
              // Inner Join
              join left in listB
             on right.Key equals left.Key
-            where right.Key % 3 == 0
+        //    where right.Key % 3 == 0
             select right).ToList();
 
 // デバッグ出力
-// Test3
-
-foreach(var obj in listTest)
-{
-    Console.WriteLine(obj.Name);
-}
+// Inner Join:,Name1,Name2,Name3,Name4,Name5
+Console.Write("Inner Join:");
+listTest.ForEach(a => Console.Write(","+a.Name));
+Console.WriteLine("");
 
 // A And B かつ 3の倍数の値のみを取得する
 var listTest2 =  ( from right in listA
@@ -42,14 +40,14 @@ var listTest2 =  ( from right in listA
              join left in listB
             on right.Key equals left.Key into tJoin
              from tj in tJoin.DefaultIfEmpty() 
-            where tj.Key % 3 == 0
-            select tj).ToList();
+        //    where tj.Key % 3 == 0
+            select right).ToList();
+
+
 
 // デバッグ出力
-// Test3
-// Test3'
+// Left Join:,Name1,Name2,Name3,Name4,Name5,Name6,Name7,Name8,Name9
+Console.Write("Left Join:");
+listTest2.ForEach(a => Console.Write(","+a.Name));
+Console.WriteLine("");
 
-foreach(var obj in listTest2)
-{
-    Console.WriteLine(obj.Name);
-}
