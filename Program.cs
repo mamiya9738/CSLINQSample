cce29ef84279f27ba4162e5579cd1be1840ab941
mamiya9738 <gogo9738@gmail.com>
2022-06-08 20:11:55 +0900

    Orderby 追加

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
 ﻿// テストデータA
 var listA = new List<A>(){
-        new A(){ Key = 1 , No = 10 , Name = "Name1",},
-        new A(){ Key = 2 , No = 10 , Name = "Name2",},
-        new A(){ Key = 3 , No = 10 , Name = "Name3",},
-        new A(){ Key = 4 , No = 10 , Name = "Name4",},
-        new A(){ Key = 5 , No = 10 , Name = "Name5",},
-        new A(){ Key = 6 , No = 20 , Name = "Name6",},
+        new A(){ Key = 1 , No = 20 , Name = "Name1",},
+        new A(){ Key = 2 , No = 30 , Name = "Name2",},
+        new A(){ Key = 3 , No = 20 , Name = "Name3",},
+        new A(){ Key = 4 , No = 30 , Name = "Name4",},
+        new A(){ Key = 5 , No = 20 , Name = "Name5",},
+        new A(){ Key = 6 , No = 30 , Name = "Name6",},
         new A(){ Key = 7 , No = 20 , Name = "Name7",},
-        new A(){ Key = 8 , No = 20 , Name = "Name8",},
+        new A(){ Key = 8 , No = 30 , Name = "Name8",},
         new A(){ Key = 9 , No = 20 , Name = "Name9",},
 };
 
@@ -51,15 +51,13 @@ Console.Write("Left Join:");
 listTest2.ForEach(a => Console.Write(","+a.Name));
 Console.WriteLine("");
 
-
-var group = (
+var group = 
         from a in listA
         group a by a.No into aGroup
-        select aGroup
-).ToList();
+        select aGroup;
 
 // デバッグ出力
-// group :,10[ ,Name1,Name2,Name3,Name4,Name5 ],20[ ,Name6,Name7,Name8,Name9 ]
+// group :,20[ ,Name1,Name3,Name5,Name7,Name9 ],30[ ,Name2,Name4,Name6,Name8 ]
 Console.Write("group :");
 foreach(var a in group)
 {
@@ -69,5 +67,20 @@ foreach(var a in group)
                 Console.Write(","+ b.Name);                
         }
         Console.Write(" ]");
+}
+Console.WriteLine("");
+
+
+var orderby = 
+        from a in listA
+        orderby a.No
+        select a;
+
+// デバッグ出力
+// orderby :[20,Name1][20,Name3][20,Name5][20,Name7][20,Name9][30,Name2][30,Name4][30,Name6][30,Name8]
+Console.Write("orderby :");
+foreach (var a in orderby)
+{
+        Console.Write("[" +a.No + ","+ a.Name + "]");                        
 }
 Console.WriteLine("");
